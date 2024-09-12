4f2235a9599d495b6b0a04a2d354fac0df2357bd
William Hleucka <william.hleucka@gmail.com>
2024-09-12 16:00:29 +0000

    add: diff template

--- app/Modules/Audit.php
+++ app/Modules/Audit.php
@@ -20,13 +20,10 @@ class Audit extends Module
             ->table("Table", "table_name")
             ->table("Table ID", "table_id")
             ->table("Field", "field")
-            ->table("Diff", "id as diff")
+            ->table("Diff", "id as audit_diff")
             ->table("Tag", "tag");
 
-        $this->format("diff", function (string $column, string $value) {
-            $record = AuditModel::find($value);
-            return $this->htmlDiff($record->old_value ?? '', $record->new_value ?? '');
-        });
+        $this->format("audit_diff", "auditDiff");
 
         $this->search("user");
 
@@ -37,43 +34,5 @@ class Audit extends Module
             ->filterLink("Update", "tag = 'UPDATE'")
             ->filterLink("Delete", "tag = 'DELETE'")
             ->filterLink("All", "1=1");
-    }
-
-    function diff($old, $new): array
-    {
-        $matrix = array();
-        $maxlen = 0;
-        foreach ($old as $oindex => $ovalue) {
-            $nkeys = array_keys($new, $ovalue);
-            foreach ($nkeys as $nindex) {
-                $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
-                    $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
-                if ($matrix[$oindex][$nindex] > $maxlen) {
-                    $maxlen = $matrix[$oindex][$nindex];
-                    $omax = $oindex + 1 - $maxlen;
-                    $nmax = $nindex + 1 - $maxlen;
-                }
-            }
-        }
-        if ($maxlen == 0) return array(array('d' => $old, 'i' => $new));
-        return array_merge(
-            $this->diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
-            array_slice($new, $nmax, $maxlen),
-            $this->diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen))
-        );
-    }
-
-    function htmlDiff($old, $new): string
-    {
-        $ret = '<div class="table-diff">';
-        $diff = $this->diff(preg_split("/[\s]+/", $old), preg_split("/[\s]+/", $new));
-        foreach ($diff as $k) {
-            if (is_array($k))
-                $ret .= (!empty($k['d']) ? "<span title='Removed' class='diff-remove'>" . implode(' ', $k['d']) . "</span> " : '') .
-                    (!empty($k['i']) ? "<span title='Added' class='diff-add'>" . implode(' ', $k['i']) . "</span> " : '');
-            else $ret .= $k . ' ';
-        }
-        $ret .= '</div>';
-        return $ret;
     }
 }
--- src/View/Format.php
+++ src/View/Format.php
@@ -2,6 +2,7 @@
 
 namespace Helios\View;
 
+use App\Models\Audit;
 use Carbon\Carbon;
 
 class Format
@@ -15,9 +16,41 @@ class Format
     public static function span(string $column,  mixed $value, array $options)
     {
         return template("components/format/span.html", [
+            ...$options,
             "column" => $column,
             "value" => $value,
-            ...$options,
         ]);
+    }
+
+    private static function diff(mixed $old, mixed $new): array
+    {
+        $matrix = array();
+        $maxlen = 0;
+        foreach ($old as $oindex => $ovalue) {
+            $nkeys = array_keys($new, $ovalue);
+            foreach ($nkeys as $nindex) {
+                $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
+                    $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+                if ($matrix[$oindex][$nindex] > $maxlen) {
+                    $maxlen = $matrix[$oindex][$nindex];
+                    $omax = $oindex + 1 - $maxlen;
+                    $nmax = $nindex + 1 - $maxlen;
+                }
+            }
+        }
+        if ($maxlen == 0) return array(array('d' => $old, 'i' => $new));
+        return array_merge(
+            self::diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+            array_slice($new, $nmax, $maxlen),
+            self::diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen))
+        );
+    }
+
+    public static function auditDiff(string $column,  mixed $value, array $options)
+    {
+        // The value is an audit id
+        $record = Audit::find($value);
+        $diff = self::diff(preg_split("/[\s]+/", $record->old_value ?? ''), preg_split("/[\s]+/", $record->new_value ?? ''));
+        return template("components/format/diff.html", ["diff" => $diff]);
     }
 }
