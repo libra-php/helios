1d92400d04c06e1adcc802a0adaf83ec5f03f14a
William Hleucka <william.hleucka@gmail.com>
2024-10-07 14:11:32 +0000

    fix: handle birth date linux

--- jobs/rotate_logs.php
+++ jobs/rotate_logs.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
-* Automatically remove old logs
-*/
+ * Automatically remove old logs
+ */
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -9,13 +10,31 @@ $logs_dir = config("paths.logs");
 $max_days = 6;
 $max_age = $max_days * 24 * 60 * 60;
 
-foreach (glob($logs_dir."/*.log") as $log) {
+foreach (glob($logs_dir . "/*.log") as $log) {
     $t = time();
-    $create_time = filectime($log);
+    $create_time = getFileCreationDateLinux($log);
     $mod_time = filemtime($log);
     $create_age = $t - $create_time;
     $mod_age = $t - $mod_time;
     if ($create_time > $max_age || $mod_age > $max_age) {
         unlink($log);
     }
+}
+
+function getFileCreationDateLinux($filePath)
+{
+    if (!file_exists($filePath)) {
+        return false;
+    }
+
+    # If you're using a file system like ext4 with the right 
+    # kernel and mount options, the birth time might be available.
+    $output = shell_exec('stat -c %W ' . escapeshellarg($filePath));
+    $creationTime = (int)$output;
+
+    if ($creationTime > 0) {
+        return date("Y-m-d H:i:s", $creationTime);
+    }
+
+    return false;
 }
