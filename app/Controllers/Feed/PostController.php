6ed405f611311269fff1dac075f3544f870fc659
William Hleucka <william.hleucka@gmail.com>
2024-10-09 22:43:58 +0000

    fix: updating post/like

--- app/Controllers/Feed/PostController.php
+++ app/Controllers/Feed/PostController.php
@@ -84,6 +84,7 @@ class PostController extends Controller
             ]);
         }
 
+        header("Hx-Trigger: like-btn");
         return $this->likeButton($id);
     }
 
@@ -116,6 +117,7 @@ class PostController extends Controller
                 ]);
                 $post->user = $user;
                 $post->user->avatar = $user->gravatar(40);
+                header("Hx-Trigger: comment-button");
                 return $this->render("admin/feed/post.html", [
                     "post" => $post,
                     "user" => $user,
