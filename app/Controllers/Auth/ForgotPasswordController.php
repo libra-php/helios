49a8913020cbc05ce866f446ed2f273f9cce3efb
William Hleucka <william.hleucka@gmail.com>
2024-11-07 15:38:44 +0000

    fix: forgot password email validation

--- app/Controllers/Auth/ForgotPasswordController.php
+++ app/Controllers/Auth/ForgotPasswordController.php
@@ -17,7 +17,7 @@ class ForgotPasswordController extends Controller
     public function post(): string
     {
         $valid = $this->validateRequest([
-            'email' => ['required'],
+            'email' => ['required', 'email'],
         ]);
 
         if ($valid) {
