edbd3c4c97e6b11ccd25ddef39b96d0bb1237002
William Hleucka <william.hleucka@gmail.com>
2024-11-25 21:27:24 +0000

    wip: email

--- config/email.php
+++ config/email.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    "enabled" => env("EMAIL_ENABLED", true),
+    "debug" => env("EMAIL_DEBUG", true),
+    "host" => env("EMAIL_HOST"),
+    "port" => env("EMAIL_PORT"),
+    "username" => env("EMAIL_USERNAME"),
+    "password" => env("EMAIL_PASSWORD"),
+    "from" => env("EMAIL_FROM"),
+    "from_name" => env("EMAIL_FROM_NAME"),
+    "reply_to" => env("EMAIL_REPLY_TO"),
+    "reply_to_name" => env("EMAIL_REPLY_TO_NAME"),
 ];
 
