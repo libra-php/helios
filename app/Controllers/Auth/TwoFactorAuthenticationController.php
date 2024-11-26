d6f3de729f13804cba419f91fd0434f9c27e4ab6
William Hleucka <william.hleucka@gmail.com>
2024-11-26 16:39:31 +0000

    wip: 2fa validation

--- app/Controllers/Auth/TwoFactorAuthenticationController.php
+++ app/Controllers/Auth/TwoFactorAuthenticationController.php
@@ -11,14 +11,24 @@ class TwoFactorAuthenticationController extends Controller
     #[Get("/two-factor-authentication", "2fa.index")]
     public function index(): string
     {
-        return $this->render("admin/two-fa/index.html", [
-        ]);
+        return $this->render("admin/two-fa/index.html", []);
     }
 
     #[Post("/two-factor-authentication", "sign-in.post")]
     public function post(): string
     {
-        die("WIP");
+        $this->addErrorMessage("min_length", "Please enter your 2FA code");
+        $this->addErrorMessage("max_length", "Please enter your 2FA code");
+        $valid = $this->validateRequest([
+            "code" => [
+                "required",
+                "min_length|6",
+                "max_length|6",
+            ]
+        ]);
+        if ($valid) {
+            die("WIP");
+        }
+        return $this->index();
     }
 }
-
